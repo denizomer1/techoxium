7eb37ac8bfd50629b5bdff9c0327825fb999227f
Omer Deniz <188370704+denizomer1@users.noreply.github.com>
2025-08-17 15:24:20 +0300

    update

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-import logo from "../public/logo.png";
-
 export const SITE_TITLE = "TechOxium";
 export const SITE_DESCRIPTION = "a blog about technology.";
